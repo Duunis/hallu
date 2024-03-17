73673d66643ebe9330cf7f16ccf1121042493065
Janne Vanhanen <janne.vanhanen@outlook.com>
2024-03-17 16:20:44 +0200

    Boom

--- main.py
+++ main.py
@@ -1,15 +1,41 @@
-from typing import Union
-
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fructose import Fructose
+from pandas import DataFrame
+from dataclasses import dataclass
+from finscraper.spiders import YLEArticle
+
+ai = Fructose()
+
+spider = YLEArticle().scrape(10)
+
+def get_articles() -> list:
+    return spider.get('list')
+
+
+@dataclass
+class Article:
+    title: str
+    body: str
+
+@ai()
+def generate_article_from_examples(articles: list) -> Article:
+    """
+    Return a new article inspired by the user provider source articles. Make it outlandish.
+    """
+
+@ai(debug=True)
+def format_article_response(article: Article) -> str:
+    """
+    Format the given article into HTML and return it as a string. Use appropriate tags for title, paragraphs and other text styles.
+    """
 
 app = FastAPI()
 
+@app.get("/", response_class=HTMLResponse)
+def get_index():
+    articles = get_articles()
+    article = generate_article_from_examples(articles)
+    html = format_article_response(article)
+    return html
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
